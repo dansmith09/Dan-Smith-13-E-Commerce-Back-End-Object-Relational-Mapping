ec1ee9c7691cecd22a67ca31d99e0f26f102337a
dansmith09 <dansmith_09@hotmail.com>
2022-08-10 13:09:53 +1000

    trying to fix heroku db again

--- config/connection.js
+++ config/connection.js
@@ -17,9 +17,6 @@ const Sequelize = require('sequelize');
 
 // Code given in JawsDB Article
 
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
